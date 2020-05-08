79f5f44693d6fc01466be8e27e9ebe5103009f24
ElAtrash <bioelatrash@gmail.com>
2020-05-08 16:22:52 +0300

    initial commit

---
+++ dist/fill-blocks.js
@@ -0,0 +1,20 @@
+function getInputValue() {
+  let inputValue = document.getElementById("user-input").value;
+  if (inputValue == null || inputValue == ""){
+    alert("Please Fill The Field!");
+  }
+  return inputValue;
+}
+
+function getRandomElement() {
+  let elementsId = ["block-1", "block-2", "block-3", "block-4", "block-5"];
+  let randomElement = elementsId[Math.floor(Math.random() * elementsId.length)];
+  return randomElement;
+}
+
+function inputValueToElement() {
+  document.getElementById(getRandomElement()).textContent=getInputValue();
+}
+
+let button = document.getElementById("button");
+button.addEventListener("click", inputValueToElement, false);
